5a488529c28fd241e4f0e3dfb7703411eb0148fe
lakkaramnaveen <lakkaramnaveen4488@gmail.com>
2021-03-02 10:38:09 +0530

    basic crud operations like add,update are done and working

--- src/main/java/com/beta/tenant/TenantApplication.java
+++ src/main/java/com/beta/tenant/TenantApplication.java
@@ -2,8 +2,11 @@ package com.beta.tenant;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
-@SpringBootApplication
+@SpringBootApplication	
+@ComponentScan("com.beta.tenant")
 public class TenantApplication {
 
 	public static void main(String[] args) {
